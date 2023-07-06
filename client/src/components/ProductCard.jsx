72122c7d2d870ae7845e993851e534ba472e6be5
osamaizhar <ahmedosamaizhar21@gmail.com>
2023-07-06 13:47:07 +0500

    added rating systems to ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -19,6 +19,25 @@ import { Link as ReactLink } from "react-router-dom";
 import { StarIcon } from "@chakra-ui/icons";
 import { useState } from "react"; // hook in react
 
+const Rating = ({ rating, numReviews }) => {
+  const { iconSize, setIconSize } = useState("14px"); // useState is not required if you are using redux
+  return (
+    <Flex>
+      <HStack spacing={"2px"}>
+        <StarIcon size={iconSize} w={"14px"} color={rating >= 2 ? "orange.500" : "gray.200"} />
+        <StarIcon size={iconSize} w={"14px"} color={rating >= 2 ? "orange.500" : "gray.200"} /> {/*checks if rating is >=2 then change star color to orange other wise leave it gray */}
+        <StarIcon size={iconSize} w={"14px"} color={rating >= 3 ? "orange.500" : "gray.200"} />
+        <StarIcon size={iconSize} w={"14px"} color={rating >= 4 ? "orange.500" : "gray.200"} />
+        <StarIcon size={iconSize} w={"14px"} color={rating >= 5 ? "orange.500" : "gray.200"} />
+      </HStack>
+      <Text fontSize={"md"} fontWeight={"bold"} ml={"4px"}>
+        {`${numReviews} ${numReviews === 1 ? "Review" : "Reviews"}`} {/*using string interpolation we are checking here if number of reviews is 1 we just say Review otherwise we say Reviews */}
+
+      </Text>
+    </Flex>
+  )
+}
+
 const ProductCard = ({ product }) => { // product is directly destructured from props to avoid redundancy
   // cards needs to be static so it does not mess up their view in mobile mode
   return (
@@ -48,12 +67,15 @@ const ProductCard = ({ product }) => { // product is directly destructured from 
         )}
       </Box>
       <Flex mt={"1"} justifyContent={"space-between"} alignContent={"center"}>
-                    {/*we use this tilted apostrophe and $ sign to add variable in sting called string interpolation */}
+        {/*we use this tilted apostrophe and $ sign to add variable in sting called string interpolation */}
         <Link as={ReactLink} to={`/products/${product._id}`} pt={"2"} cursor={"pointer"}> {/*pt is padding top  setting the mouse cursor as "pointer" so that user knows they can click on the card */}
           <Box fontSize={"2xl"} fontWeight={"semibold"} lineHeight={"tight"}>
             {product.name}
           </Box>
         </Link> {/*this will give route using product id */}
+      </Flex>
+      <Flex>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
       </Flex>
       <Flex justify={"space-between"}>               {/*if we are using light more color of text will be gray otherwise it will be white */}
         <Box fontSize={"2xl"} color={useColorModeValue("gray.800", "white")}>
